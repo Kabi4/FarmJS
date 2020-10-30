4e5a75b0d8c1f699dea044fd224c8c7fbf2aa834
Kushagra Singh <59528754+Kabi4@users.noreply.github.com>
2020-10-30 15:28:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const http = require("http");
 
 const fs = require("fs");
 
+const url = require("url");
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/Template-Overview.html`,"utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/Template-Card.html`,"utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/Template-Product.html`,"utf-8");   
+const tempProduct = fs.readFileSync(`${__dirname}/templates/Template-Product.html`,"utf-8");
 
 const fileData = fs.readFileSync(`${__dirname}/dev-data/data.json`,"utf-8");
 const data = JSON.parse(fileData);
@@ -14,6 +16,10 @@ function replaceStringCard(prodect,cardHtml) {
     result = result.replace(/{%PRODUCT_NAME%}/g,prodect.productName);
     result = result.replace(/{%PRICE%}/g,prodect.price);
     result = result.replace(/{%PRODUCT_ID%}/g,prodect.id);
+    result = result.replace(/{%ORIGIN%}/g,prodect.from);
+    result = result.replace(/{%DESCRIPTION%}/g,prodect.description);
+    result = result.replace(/{%CONTENTS%}/g,prodect.nutrients);
+    result = result.replace(/{%QUANTITY%}/g,prodect.quantity);
     if(prodect.organic===false){
         result = result.replace(/{%NOT_ORGANIC%}/g,"not-organic");
     }
@@ -21,9 +27,15 @@ function replaceStringCard(prodect,cardHtml) {
     return result;
 }
 
+
 const server = http.createServer((req,res)=>{
-    const path = req.url;
-    if(path==="/"){
+    const {query,pathname} = url.parse(req.url,true);
+    if (pathname==="/product"){
+        const product = data[query.id];
+        const productHtml = replaceStringCard(product,tempProduct);
+        res.writeHead(200,{"Content-type": "text/html"});
+        res.end(productHtml);
+    }else if(pathname==="/"){
         const cards = data.map(ele=>{
             return replaceStringCard(ele,tempCard);
         });
